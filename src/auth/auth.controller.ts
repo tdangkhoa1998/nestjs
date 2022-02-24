23112a18d627198de090647cc22eed2e4c7fb372
Trần Đăng Khoa <48779919+tdangkhoa1998@users.noreply.github.com>
2022-02-24 14:37:41 +0700

    coding new feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
@@ -13,6 +13,7 @@ export class AuthController {
         return this.authService.signUp(dto);
     }
 
+    @HttpCode(HttpStatus.OK)
     @Post('signIn')
     signIn(@Body() dto: AuthDto) {
         return this.authService.signIn(dto);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from "express";
-
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
-
-    @UseGuards(AuthGuard("jwt"))
     @Get('me')
-    getMe(@Req() req: Request) {
-        return req.user;
+    getMe(@GetUser() user: User) {
+        return user;
     }
-
 }
